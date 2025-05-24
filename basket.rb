d1be314f767552efa14f95834012fcac643b9f10
Mohammed Ghonmi <mohammedghonmi@gmail.com>
2025-05-24 12:24:12 +0300

    Implement the abililty of basket to add products

--- basket.rb
+++ basket.rb
@@ -1,6 +1,13 @@
 class Basket
-  def add(code)
 
+  def initialize(product_catalogue:)
+    @catalogue = product_catalogue
+    @items = []
+  end
+
+  def add(code)
+    raise "Invalid code: #{code}" unless @catalogue.key?(code)
+    @items << @catalogue[code].dup
   end
 
   def total
--- product.rb
+++ product.rb
@@ -1,5 +1,9 @@
 class Product
+  attr_reader :code, :name, :price
+
   def initialize(code:, name:, price:)
-    
+    @code = code
+    @name = name
+    @price = price
   end
 end
