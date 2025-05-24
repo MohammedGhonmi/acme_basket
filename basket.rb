0abdb1f98572e2857c281cf6868c4b1d444d434e
Mohammed Ghonmi <mohammedghonmi@gmail.com>
2025-05-24 12:31:08 +0300

    Implement total basket with the addition of offers and delivery rules

--- basket.rb
+++ basket.rb
@@ -1,15 +1,20 @@
 class Basket
-  def initialize(product_catalogue:)
+  def initialize(product_catalogue:, delivery_rule:, offers: [])
     @catalogue = product_catalogue
+    @delivery_rule = delivery_rule
+    @offers = offers
     @items = []
   end
 
   def add(code)
-    raise "Invalid code: #{code}" unless @catalogue.key?(code)
+    raise "Invalid product code: #{code}" unless @catalogue.key?(code)
     @items << @catalogue[code].dup
   end
 
   def total
-
+    applied_items = @offers.reduce(@items.dup) { |acc, offer| offer.apply(acc) }
+    subtotal = applied_items.sum(&:price).round(2)
+    delivery = @delivery_rule.charge(subtotal)
+    (subtotal + delivery).round(2)
   end
 end
