a1e90f8cf9f4f94e1266f332117814e5339e95f5
Mohammed Ghonmi <mohammedghonmi@gmail.com>
2025-05-24 12:27:20 +0300

    Add delivery rules implementation

--- delivery_rule.rb
+++ delivery_rule.rb
@@ -1,9 +1,12 @@
 class DeliveryRule
   def initialize(thresholds)
-    
+    @thresholds = thresholds.sort_by(&:first)
   end
 
   def charge(subtotal)
-
+    @thresholds.each do |limit, fee|
+      return fee if subtotal < limit
+    end
+    0.0
   end
 end
